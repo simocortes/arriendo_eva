175418ebb2dc0f7bd61b10315bc463bb3a888c72
Your Name <simocortesr@gmail.com>
2024-08-25 21:02:23 -0400

    [ADD] se crea index y profile. ademas de agregar views, urls. y actualizar templates y services

--- arriendo_m7/urls.py
+++ arriendo_m7/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from arriendo_m7.views import index, register, profile
+	
 urlpatterns = [
-    path()
+    path('', index, name='index'),
+    path('accounts/register/', register, name='register',),
+    path('accounts/profile/', profile, name='profile',),
 ]
--- arriendo_m7/views.py
+++ arriendo_m7/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from arriendo_m7.services import crear_user, editar_user_sin_password
+from django.contrib.auth.decorators import login_required
+from arriendo_m7.models import Inmueble
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',)
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        direccion = request.POST['direccion']
+        telefono = request.POST['telefono']
+        rol = request.POST['rol']
+        password = request.POST['password']
+        password_repeat = request.POST['password_repeat']
+            
+        crear = crear_user(request, username, first_name, last_name, email, password, password_repeat, direccion, rol, telefono)
+
+        if crear: # Si crear es True
+            return redirect('/accounts/login') 
+        # Si llegó, crear fue False
+        return render(request, 'registration/register.html', {
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'direccion': direccion,
+            'telefono': telefono,
+            'rol': rol,
+            })
+    else: # en caso de metodo GET
+        return render(request, 'registration/register.html')
+    
+
+
+@login_required
+def profile(request):
+    id_usuario = request.user.id
+    propiedades = Inmueble.objects.filter(propietario_id=id_usuario)
+    context = {
+        'propiedades': propiedades
+    }
+
+    if request.method == 'POST':
+        if request.POST['telefono'].strip() != '':
+            username = request.user
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            direccion = request.POST['direccion']
+            telefono = request.POST['telefono']
+            rol = request.POST['rol']
+            editar_user_sin_password(username, first_name, last_name, email, direccion, rol, telefono)
+            messages.success(request, 'Ha actualizado sus datos con exito')
+            return redirect('/accounts/profile')
+        else:
+            username = request.user
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            email = request.POST['email']
+            direccion = request.POST['direccion']
+            rol = request.POST['rol']
+                        
+            editar_user_sin_password(username, first_name, last_name, email, rol, direccion)
+            messages.success(request, 'Ha actualizado sus datos con exito sin telefono')
+            return redirect('/accounts/profile')
+    else:
+        return render(request, 'profile.html', context)
